039a77f6304b2bda2788957864460e5e6668ff80
Caroline <caroline@hartsmartproducts.com>
2023-12-23 13:58:23 -0600

    Update config url

--- hsp1_config_tool.sh
+++ hsp1_config_tool.sh
@@ -17,7 +17,7 @@ PKG_COUNT="${PKG_MANAGER} -s -o Debug::NoLocking=true upgrade | grep -c ^Inst ||
 
 # Repo URLs/Locations
 TOOL_GIT_URL="https://github.com/HartSmart-Products/HSP1-I-Config-Tool.git"
-CONFIG_GIT_URL=""
+CONFIG_GIT_URL="https://github.com/HartSmart-Products/HSP1-I-SD-Image.git"
 USTREAMER_GIT_URL="https://github.com/pikvm/ustreamer.git"
 REPO_TEMP_DIR="/home/pi/.hsp/temp/repos"
 TOOL_GIT_DIRECTORY="${REPO_TEMP_DIR}/hsp1-i-config-tool"
